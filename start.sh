da0d10dd9f373dadc9711f18cc7e496824a0cedd
root <root@ip-10-0-0-17.ap-southeast-2.compute.internal>
2017-11-03 04:11:07 +0000

    Install BitBucket+Nexus+Confluence

--- start.sh
+++ start.sh
@@ -11,3 +11,9 @@
 docker volume create --name bitbucketVolume
 docker run -v bitbucketVolume:/var/atlassian/application-data/bitbucket --name="bitbucket" -d -p 7990:7990 -p 7999:7999 atlassian/bitbucket-server
 
+#Install Nexus
+#docker run -d -p 8081:8081 --name nexus sonatype/nexus3
+
+#Install Confluence
+#docker run -v /data/your-confluence-home:/var/atlassian/application-data/confluence --name="confluence" -d -p 8090:8090 -p 8091:8091 atlassian/confluence-server
+
