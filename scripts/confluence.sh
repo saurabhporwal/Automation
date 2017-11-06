fe4524e42b6815cb58658cab4d5b37765895da76
root <root@ip-10-0-0-227.ap-southeast-2.compute.internal>
2017-11-06 05:26:04 +0000

    Final checkin

--- scripts/confluence.sh
+++ scripts/confluence.sh
@@ -1,2 +1,2 @@
 #Install Confluence
-#docker run -v /data/your-confluence-home:/var/atlassian/application-data/confluence --name="confluence" -d -p 8090:8090 -p 8091:8091 atlassian/confluence-server
+docker run -v /data/your-confluence-home:/var/atlassian/application-data/confluence --name="confluence" -d -p 8090:8090 -p 8091:8091 atlassian/confluence-server > /data/remote/confluence_docker.log
--- scripts/nexus.sh
+++ scripts/nexus.sh
@@ -1,2 +1,2 @@
 #Install Nexus
-#docker run -d -p 8081:8081 --name nexus sonatype/nexus3
+docker run -d -p 8081:8081 --name nexus sonatype/nexus3
